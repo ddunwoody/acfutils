4e90888040819a8a71e70af0f770bc43c23c6db7
David Dunwoody <david.dunwoody@gmail.com>
2023-08-03 15:23:13 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# acfutils
+Rust wrapper around [`libacfutils`](https://github.com/skiselkov/libacfutils)
+
+Intended to support usage of [`libelec`](https://github.com/skiselkov/libelec) and (eventually) [`libradio`](https://github.com/skiselkov/libradio) from Rust.
---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+/*
+ * Copyright (c) 2023 David Dunwoody.
+ *
+ * All rights reserved.
+ */
+#![deny(clippy::all)]
+#![warn(clippy::pedantic)]
+
+use nanorand::{Rng, WyRand};
+use std::ffi::{c_char, CStr, CString};
+use tracing::info;
+
+pub fn crc64_init(seed: Option<u64>) {
+    unsafe {
+        acfutils_sys::crc64_init();
+        let seed = seed.unwrap_or(WyRand::default().generate());
+        acfutils_sys::crc64_srand(seed);
+    }
+}
+
+pub fn log_init(prefix: &str) {
+    unsafe {
+        cstring(prefix, |p| acfutils_sys::log_init(Some(log), p));
+    }
+}
+
+extern "C" fn log(message: *const c_char) {
+    unsafe {
+        info!("{}", CStr::from_ptr(message).to_string_lossy());
+    }
+}
+
+fn cstring(value: &str, f: fn(*const c_char)) {
+    let c_value = CString::new(value)
+        .unwrap_or_else(|e| panic!("Could not create CString from '{value}': {e}"));
+    f(c_value.as_ptr());
+}
